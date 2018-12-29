136e8cc3c2873df857c887e8cff39970b9c99358
Isilvorn <isilvorn@publicmail.biz>
2018-12-29 09:56:47 -0500

    Added wdata source and include files.

---
+++ include/wdata.h
@@ -0,0 +1,53 @@
+#include <list>
+#include <iomanip>
+
+#include "../include/vect.h"
+
+#ifndef WDATA_H
+#define WDATA_H
+
+/*
+** The "wdata" struct contains all of the mutable data pertaining to a word vector.
+** any data contained in this structure is guaranteed not to affect the sort order
+** in the multiset (binary search tree) used to contain the dictionary of words
+** used in the data set.  It is dynamically allocated at the time of a wordvect
+** instance creation.
+*/
+struct wdata {
+public:
+  wdata(void);                  // default constructor
+  ~wdata(void);                 // destructor (does nothing)
+
+  
+  void clear(int=0);            // clear all data in the structure and specify Svect nominal size
+  void clearf(void);            // clears the features vector collection only
+  void copy(const wdata&);      // copy the data from one instance of the structure to another
+  void add(Svect&);             // add an example to the precursor data
+  Svect& front(void);           // access the front element in the precursor data
+  Svect& next(void);            // access the next element in the precursor data
+  int    size(void);            // access the size of the precursor data
+  void incr(void);              // increment the usage count
+  int  count(void) const;       // return the usage count
+  void init_weights(double);    // initializes the weights used for the logistic regression calculation
+  void init_logr(double);       // initializes the features array used in the logistic regression
+  void add_negs(list<Svect>&);  // adds negative observation data to the features and observations
+  void pad_features();          // pads the features vector with empty vectors
+  void disp_features(void);     // displays all of the data in the features vector
+  void disp_obs(void);          // displays all of the data in the observations vector
+  void disp_weights(void);      // displays the weights
+  double find_prob(Svect&);     // given a vector of precursors, finding probability
+                                // that the word in this instance is the next one
+
+  list<Svect> prec;             // data set for precursors
+  list<Svect>::iterator lit;    // the persistent list iterator
+  int         ct;               // usage count
+  int         sz;               // nominal size of the sparse vectors
+  int         fmax;             // max size of the features vector (set to 10x prec size)
+  int         fsize;            // current size of the features vector
+  Svect       weights;          // the weights calculated via logistic regession for predicting
+                                // this word based on its list of precursor words
+  Svect      *features;         // the features vector collection used in the logistic regression
+  Svect       obs;              // the observations vector used in the logistic regression
+};
+
+#endif // WDATA_H
---
+++ src/wdata.cpp
@@ -0,0 +1,229 @@
+
+#include "../include/wdata.h"
+#include "../include/dict.h"
+
+/*
+******************************************************************************
+******************* wdata STRUCT DEFINITION BELOW HERE ***********************
+******************************************************************************
+*/
+
+/*
+** The default constructor uses the clear() function to initialize the mutable 
+** word data.
+*/
+wdata::wdata(void)  { 
+  features = nullptr; 
+  clear(); 
+} // end default constructor
+
+/*
+** Destructor (does nothing).
+*/
+wdata::~wdata(void) { }
+
+/*
+** The clear() function initializes all data in the wdata struct.  Any data 
+** currently residing in the struct is lost.
+*/
+void wdata::clear(int s) { 
+  ct = 0;
+  sz = s;
+  prec.erase(prec.begin(), prec.end());
+  weights.resize(s);
+  clearf();
+} // end clear()
+
+/*
+** The clearf() function initializes the features data.  This array is only
+** ever meant to be a temporary container to hold the features used in the
+** logistic regression calculation.  Once the weights are obtained, this
+** data may be discarded.
+*/
+void wdata::clearf(void) {
+  if (features != nullptr) delete features;
+  features = nullptr;
+} // end clearf()
+
+/*
+** The copy() function copies all data from another wdata struct to this
+** instance.  Used in the assignment operator and the copy constructor.
+*/
+void wdata::copy(const wdata &wd) { 
+  list<Svect>::const_iterator it;
+  ct = wd.ct;
+  sz = wd.sz;
+  it = wd.prec.cbegin(); 
+  while (it != wd.prec.cend()) { 
+    prec.push_front(*it); 
+    it++; 
+  } // end while (it)
+} // end copy()
+
+/*
+** The add() function adds a vector to the precursor data.  This data will be
+** used as a list of examples to run the logistic regression on.
+*/
+void wdata::add(Svect &v) { 
+  prec.push_front(v); 
+} // end add()
+
+/*
+** The front() function returns a reference to the first element of the
+** precursor data.  The "lit" iterator is a persistent iterator that will
+** be used in the next() function; it is reset to the beginning of the list
+** when the front() function is called.
+*/
+Svect& wdata::front(void) {
+  lit=prec.begin();
+  return *lit;
+} // end front()
+
+/*
+** The next() function returns a reference to the next element of the
+** precursor data that is pointed to by the "lit" iterator after it is
+** incremented.
+*/
+Svect& wdata::next(void) {
+  lit++; 
+  if (lit == prec.end()) lit=prec.begin(); 
+  return *lit;
+}
+
+/*
+** The size() function returns the size of the precursor list (# of elements).
+*/
+int wdata::size(void) { return prec.size(); }
+
+/*
+** The incr() function increments the usage count of the word.
+*/
+void wdata::incr(void) { ct++; }
+
+/*
+** The count() function returns the usage count of the word.
+*/
+int wdata::count(void) const { return ct; }
+
+/*
+** The init_weights() function initializes the weights used in the logistic
+** regression.  It first adds in all of the data from each precursor so that
+** there is an explicit element in the weights vector for every explicit
+** element used in the precursor list.  It then replaces the data for each
+** explicit element with an initial weight (supplied in the argument).
+*/
+void wdata::init_weights(double w) { 
+  list<Svect>::iterator it;
+
+  // nothing to do if there is no precursor data
+  if (prec.begin() == prec.end()) return; 
+
+  it = prec.begin(); 
+  weights = *it;
+  it++; 
+  while (it != prec.end()) { 
+    weights += *it; 
+    it++; 
+  } // end while (lit);
+  weights = w; // overwrite all of the explicit data with the initial weight supplied
+} // end init_weights()
+
+
+/*
+** The init_logr() function initializes the data used in the logistic 
+** regression.
+*/
+void wdata::init_logr(double w) {
+  list<Svect>::iterator it;
+  int i = 0; 
+
+  if (w != 0) init_weights(w);
+
+  clearf();                          // clear any features that already exist
+  fmax = 10*prec.size();             // setting max size of features vector
+  features = new Svect[fmax];        // allocate a new array of vectors
+  obs.resize(prec.size());           // clear and size the observations vector
+  obs.setall(1);                     // set all of these observations to 1
+
+  it = prec.begin();
+  while (it != prec.end()) {         // copy precursor data to features
+    features[i++] = *it; 
+    it++; 
+  } // end while (it)
+
+  fsize = i;
+} // end init_logr()
+
+/*
+** The add_negs() function adds the data for the negative observations to the
+** features and observations data sets.  The negative observations are based on 
+** a random sampling of the rest of the data; this needs to be appended to the 
+** features and observations data so that an accurate model can be derived.  The 
+** positive observations should be relatively small in size compared to the 
+** negative observations in order to yield an accurate regression.
+*/
+void wdata::add_negs(list<Svect> &negs) {
+  list<Svect>::iterator it;
+  int i = fsize;
+
+  it = negs.begin();
+  while (it != negs.end()) {
+    if (i >= fmax) { fsize = fmax; return; }
+    features[i++] = *it;
+    it++;
+  } // end while (it)
+
+  obs.upsize(i);
+  fsize = i;
+} // end add_negs()
+
+/*
+** The disp_features() vector displays all of the data in the features vector.
+*/
+void wdata::disp_features(void) {
+
+  cout << "Features:" << endl;
+  for (int i=0; i < fsize; i++) {
+    cout << setprecision(0) << fixed << features[i] << setprecision(4) 
+        << fixed << " (" << find_prob(features[i]) << ")" << endl;
+  } // end for (i)
+} // end disp_features()
+
+/*
+** The disp_obs() function displays all of the data in the observations vector.
+*/
+void wdata::disp_obs(void) {
+  cout << "Obervations:" << endl;
+  cout << setprecision(0) << fixed << "[ ";
+  for (int i=0; i < obs.size(); i++) cout << obs[i] << " ";
+  cout << "] (" << obs.size() << ")" << endl;
+} // end disp_obs()
+
+/*
+** The disp_weights() function displays all of the data in the weights vector.
+*/
+void wdata::disp_weights(void) {
+  cout << "Weights:" << endl;
+  cout << setprecision(4) << fixed << weights << " (" << weights.size() << ")" << endl;
+} // end disp_weights()
+
+/*
+** The pad_features() function pads the features vector so that there are
+** enough dummy features to satisfy the matrix algebra routines.
+*/
+void wdata::pad_features(void) {
+  Svect pad(weights.size());
+  for (int i=fsize; i < fmax; i++) { features[i] = pad; } 
+  obs.upsize(fmax);
+}
+
+/*
+** The find_prob() function finds the probability that the word in this
+** instance is the next one.  The return value is the probability.
+*/
+double wdata::find_prob(Svect &p) {
+  double               wTx;
+
+  wTx  = (weights * p).sum();
+  return exp(wTx)/(1 + exp(wTx));
+}
